d35bbb970b508933f663cd5f342c04f2bc71c5fc
shloimi minkowicz <shloimimink@gmail.com>
2019-05-19 19:49:36 -0700

    node

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,5 +14,4 @@ export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_REPOS = 'GET_REPOS';
-export const DELETE_REPOS = 'DELETE_REPOS';
 
