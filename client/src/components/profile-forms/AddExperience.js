4e3792e2b8c7bc5ced23ed708e1eb24d2522afea
shloimi minkowicz <shloimimink@gmail.com>
2019-05-19 22:05:14 -0700

    profile update

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useState} from 'react';
-import {Link, wihtRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import {addExperience} from "../../actions/profile";
@@ -85,4 +85,8 @@ AddExperience.propTypes = {
     addExperience: PropTypes.func.isRequired
 };
 
-export default connect(null, {addExperience})(AddExperience);
+export default connect(
+    null,
+    {addExperience}
+)(withRouter(AddExperience));
+
